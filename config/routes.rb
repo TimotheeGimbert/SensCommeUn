bd5b4e4d9349b405c1b33618c12c992b7368cb3a
FabCom <fabcom@communs.fr>
2021-12-08 20:08:23 +0100

    Add Devise for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
   root "pages#index"
+
+  namespace :admin do
+    get 'dashboard/index'
+  end
+
+  devise_for :admins
+  
   get 'pages/presentation'
   get 'pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
