b443e50551567fcf61f8c500709b757c18ee796a
Louis-Lorgere <louis.lorgere@gmail.com>
2021-12-09 23:10:32 +0100

    add view for user dashboards with controller models

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,6 +2,23 @@ class Admin::DashboardsController < ApplicationController
   before_action :has_admin_rights? #That is a double security, as this controller's access is already locked by the devise_scope
 
   def index
+    @render_view = ""
+    if params[:panel] == "Gestions des utilisateurs"
+      @render_view = "user/partials/list"
+      @users = User.all
+    elsif params[:panel] == "Gestion des Organisations"
+      @render_view = "organizations/list"
+      @organizations = Organization.all
+    elsif params[:panel] == "Création d'une organisation"
+      @render_view = 'organizations/form'
+      @organization = Organization.new
+    elsif params[:panel] == "Messagerie"
+      @render_view = 'private_messages/list'
+      @private_messages = current_admin.private_messages
+    else
+      @render_view = "organizations/list"
+      @organizations = Organization.all
+    end
   end
 
 end
