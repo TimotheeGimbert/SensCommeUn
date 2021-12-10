066dd7d60b03a5cf112f9b8cec79f420094b1785
TimotheeGimbert <timothee.gimbert@free.fr>
2021-12-10 16:43:07 +0100

    completes leg_rep controller accesses

--- app/controllers/join_messages_recipients_controller.rb
+++ app/controllers/join_messages_recipients_controller.rb
@@ -1,7 +1,7 @@
 class JoinMessagesRecipientsController < ApplicationController
   before_action :set_join_messages_recipient, only: %i[ show edit update destroy ]
   before_action :has_user_rights?, only: %i[ new create destroy ]
-  before_action :has_admin_rights?, only: %i[ index show edit create update destroy ]
+  before_action :has_admin_rights?, only: %i[ index show edit update ]
 
   # GET /join_messages_recipients or /join_messages_recipients.json
   def index
