15a7f8e0fa811d8ff2a5de7a814642da76b51427
FabCom <fabcom@communs.fr>
2021-12-17 11:44:14 +0100

    Bug fixed for profile creation when phone number is null

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def format_phone_number()
+    if phone_number
+      unless phone_number.match?(/\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/)
+        errors.add(:phone_number, "Numéro non valide en France")
+      end
+    end
+  end
 end
